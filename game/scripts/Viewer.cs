fbc56f544c72d1aa0408d75ad32c4258e7eb1331
redthing1 <redthing1@alt.icu>
2023-08-01 17:14:26 -0700

    call game loader

--- game/scripts/Viewer.cs
+++ game/scripts/Viewer.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
+using SolarisDIB.Cli.Helpers;
+using SolarisDIB.Cli.Util;
+using SolarisViewer;
 
-public partial class Test1 : Node
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+public partial class Viewer : Control {
+    private GodotLogger _logger;
+    private GameLoadHelper.LoadedGame _loadedGame;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        var args = OS.GetCmdlineUserArgs();
+        if (args.Length < 1) {
+            GD.PrintErr("Usage: pass <gameId> as first argument");
+            GetTree().Quit();
+        }
+
+        var gameId = args[0];
+        GD.Print($"Creating viewer for game {gameId}");
+
+        _logger = new GodotLogger(Verbosity.Trace);
+
+        _loadedGame = GameLoadHelper.LoadOfflineGameDataInteractive(_logger, gameId).Result;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
 }
